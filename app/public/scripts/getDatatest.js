cf6e5a3adc32b6eea331560c59e68b0426591cf4
LuisSaberon <lsaberon@my.bcit.ca>
2025-03-04 21:42:24 -0800

    Added event listeners to load-content for answering questions

--- app/public/scripts/getDatatest.js
+++ app/public/scripts/getDatatest.js
@@ -1,6 +1,14 @@
-document.addEventListener("DOMContentLoaded", function (){
-  const el = document.querySelectorAll('[data-value = "1"]');
-  el.addEventListener("click", function () {
-    console.log("hello");
+export function listen() {
+  document.addEventListener("DOMContentLoaded", function (){
+
+
+  const el = document.querySelectorAll('.survey-card-container');
+  // console.log(el[0]);
+  // while (el[0].innerHTML == "") {
+  // }
+  console.log(el);
+  el.forEach((elem) => {
+    console.log(elem.children);
   });
 });
+};
--- app/public/scripts/load_content.js
+++ app/public/scripts/load_content.js
@@ -3,7 +3,17 @@ export function loadContent(selector, filePath) {
         fetch(filePath)
             .then(response => response.text())
             .then(html => element.innerHTML = html)
+            .then(html => getDataTest(html))
             .catch(error => console.error(`Error loading ${filePath} into ${selector}:`, error));
     });
 }
 
+function getDataTest(html) {
+    const el = document.querySelectorAll('[data-value]');
+    console.log(el);
+    el.forEach( (currentValue, currentIndex) => {
+        currentValue.addEventListener("click", () => {
+            console.log("");
+        });
+    });
+}

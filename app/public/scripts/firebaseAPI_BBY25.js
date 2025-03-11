565a04c15de733ca180b63324671570c07dd84a3
ShivaunBartoo <shivaunbartoo@yahoo.com>
2025-03-11 08:44:28 -0700

    updated .gitignore

--- app/public/scripts/firebaseAPI_BBY25.js
+++ app/public/scripts/firebaseAPI_BBY25.js
@@ -2,18 +2,17 @@
 //  Your web app's Firebase configuration
 //----------------------------------------
 var firebaseConfig = {
-  apiKey: "AIzaSyARBCTTJO4ke548pHGNCzCudFqmEm7suHw",
+    apiKey: "AIzaSyARBCTTJO4ke548pHGNCzCudFqmEm7suHw",
 
-  authDomain: "bby25-likemind.firebaseapp.com",
+    authDomain: "bby25-likemind.firebaseapp.com",
 
-  projectId: "bby25-likemind",
+    projectId: "bby25-likemind",
 
-  storageBucket: "bby25-likemind.firebasestorage.app",
+    storageBucket: "bby25-likemind.firebasestorage.app",
 
-  messagingSenderId: "286236329435",
+    messagingSenderId: "286236329435",
 
-  appId: "1:286236329435:web:a44658366321632dcf13fe"
-
+    appId: "1:286236329435:web:a44658366321632dcf13fe",
 };
 
 //--------------------------------------------

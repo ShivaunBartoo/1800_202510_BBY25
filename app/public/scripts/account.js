cb617c549e6eb434fcf928f88be664af0b5cc553
LuisSaberon <lsaberon@my.bcit.ca>
2025-03-25 19:11:10 -0700

    Added view of past answers on account page

--- app/public/scripts/account.js
+++ app/public/scripts/account.js
@@ -7,8 +7,30 @@ async function initialize() {
     let user = await getUserData();
 
     if (user) {
-        document.querySelector("#bigName").innerHTML = user.data().name;
-        document.querySelector("#profile-picture-large").setAttribute("src", user.data().profilePhoto);
+        let udata = user.data();
+        document.querySelector("#bigName").innerHTML = udata.name;
+        document.querySelector("#name").innerHTML = udata.name;
+        document.querySelector("#profile-picture-large").setAttribute("src", udata.profilePhoto);
+        document.querySelector("#email").innerHTML = udata.email;
+        document.querySelector("#contact-method").innerHTML = udata.contactMethod;
+        document.querySelector("#contact-info").innerHTML = udata.contactInfo;
+        
+        for (let interest in udata.interests) {
+            let score = udata.interests[interest];
+            switch(score){
+                case 2: document.querySelector("#very-interested").innerHTML += `<span class="noun-bubble">${interest}</span>`;
+                break;
+                case 1: document.querySelector("#mildly-interested").innerHTML +=`<span class="noun-bubble">${interest}</span>`;
+                break;
+                case 0: document.querySelector("#no-opinion").innerHTML +=`<span class="noun-bubble">${interest}</span>`;
+                case -1: document.querySelector("#mildly-disinterested").innerHTML +=`<span class="noun-bubble">${interest}</span>`;
+                case -2: document.querySelector("#very-disinterested").innerHTML +=`<span class="noun-bubble">${interest}</span>`;
+
+            }
+
+            // if (udata.interests[interest] === 2) {
+            // }
+        }
     } else {
         window.location.href = "./404.html";
         console.log("Error: User not found");
